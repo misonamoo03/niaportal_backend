67455f9cc70bc72ff17698ac0f68dfe7be695dd4
김진호 <jin-ho0104@ucom.kr>
2021-01-11 17:58:44 +0900

    VO 수정

---
+++ niaportal/src/main/java/com/misonamoo/niaportal/domain/User.java
@@ -0,0 +1,24 @@
+package com.misonamoo.niaportal.vo;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@ToString
+public class User {
+    private int userNo
+    private String email;
+    private String password;
+    private String userName;
+    private String tel;
+    private String agency;
+    private String companyTypeCode;
+    private String dwConfirmYN;
+    private Date regDate;
+    private Date updDate;
+}

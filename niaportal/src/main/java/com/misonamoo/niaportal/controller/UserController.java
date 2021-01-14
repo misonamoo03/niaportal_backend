d4aa3d38aa633cb11ab9786167399afcfec82350
gnsalswjddma96 <gnsalswjddma96@nexmotion.co.kr>
2021-01-14 19:36:12 +0900

    회원 CRUD

--- niaportal/src/main/java/com/misonamoo/niaportal/controller/UserController.java
+++ niaportal/src/main/java/com/misonamoo/niaportal/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.misonamoo.niaportal.controller;
 
+import com.misonamoo.niaportal.common.CommonUtil;
 import com.misonamoo.niaportal.common.SHA256Util;
 import com.misonamoo.niaportal.domain.PwSec;
 import com.misonamoo.niaportal.domain.User;
@@ -17,9 +18,12 @@ import javax.mail.internet.MimeMessage;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
+import static com.misonamoo.niaportal.common.CommonUtil.*;
 
 @RestController
 @RequestMapping("/User")
@@ -33,128 +37,265 @@ public class UserController {
     @Autowired
     PwSecService pwSecService;
 
+    @Value("${key}")
+    private String salt;    // 비밀번호 암호화 키
+
     //회원가입
     @PostMapping(value = "/register")
-    public Map<String, String> register(@RequestBody User user) throws Exception {
-        Map<String, String> ret = new HashMap();
-        ret.put("code", "200");
-        int emailCnt = userService.dupEmail(user);
-        if (emailCnt == 0) {
+    public Map<String, Object> register(@RequestBody User user) throws Exception {
+        Map<String, Object> ret = new HashMap();
+        ret.put("code", 200);
+        ret.put("message", "회원가입 정상 처리");
+        int emailCnt = userService.dupEmail(user);  // 0이면 이메일 중복 x, 1이면 이메일 중복
+        if (isNotNull(user.getEmail()) ||    // 필수 변수값 없음
+            isNotNull(user.getEmail()) ||
+            isNotNull(user.getEmail())||
+            isNotNull(user.getEmail()) ||
+            isNotNull(user.getEmail()) ||
+            isNotNull(user.getEmail())) {
+
+            ret.put("code", 100);
+            ret.put("message", "필수 변수값 없음");
+        }
+        else if (emailCnt > 0) {
+            ret.put("code", 101);
+            ret.put("message", "중복된 ID");
+        }
+        else {
             // 회원가입처리
             userService.register(user);
-        } else {
-            ret.put("code", "102");
-            ret.put("message", "중복된 ID");
         }
         return ret;
     }
 
     //회원삭제
     @PostMapping(value = "/delete")
-    public Map<String, String> delete(@ModelAttribute User user) throws Exception {
-        Map<String, String> ret = new HashMap();
-        ret.put("code", "200");
+    public Map<String, Object> delete(@ModelAttribute User user, HttpServletResponse response, HttpServletRequest request) throws Exception {
+        Map<String, Object> ret = new HashMap();
+        ret.put("code", 200);
+        ret.put("message", "회원삭제 정상 처리");
+        if (CommonUtil.isNotNull(user.getEmail())) {
+            ret.put("code", 100);
+            ret.put("message", "필수 변수값 없음");
+            return ret;
+        }
+        String emailCookie = "";
         //쿠키정보를 체크해서 회원인지를 확인한다. user.email과 쿠키에 있는 이메일이 같은지 확인하다.
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie c : cookies) {
+                if ("email".equals(c.getName())) {
+                    emailCookie = URLDecoder.decode(c.getValue(), "UTF-8");
+                }
+            }
+        }
+
         int emailCnt = userService.dupEmail(user);
         if (emailCnt > 0) {
-            //회원탈퇴처리
-            userService.delete(user);
-            //회원탈퇴가 제대로 처리 되면 쿠키를 삭제한다.
-        } else {
-            ret.put("code", "101");
-            ret.put("message", "회원정보 없음");
+            if(user.getEmail().equals(emailCookie)) {
+                //회원삭제처리
+                userService.delete(user);
+                //회원삭제가 제대로 처리 되면 쿠키를 삭제한다.
+                if (cookies != null) { // 쿠키가 한개라도 있으면 실행
+                    for (Cookie c : cookies) {
+                        c.setMaxAge(0); // 유효시간을 0으로 설정
+                        c.setPath("/");
+                        response.addCookie(c); // 응답 헤더에 추가
+                    }
+                }
+            }
+            else {
+                ret.put("code", 104);
+                ret.put("message", "접근권한 없음");
+            }
+        }
+        else {
+            ret.put("code", 102);
+            ret.put("message", "아이디 없음");
         }
         return ret;
     }
 
     //회원탈퇴
     @PostMapping(value = "/withdraw")
-    public Map<String, String> withdraw(@ModelAttribute User user) throws Exception {
-        Map<String, String> ret = new HashMap();
-        ret.put("code", "200");
+    public Map<String, Object> withdraw(@ModelAttribute User user, HttpServletResponse response, HttpServletRequest request) throws Exception {
+        Map<String, Object> ret = new HashMap();
+        ret.put("code", 200);
+        ret.put("message", "회원탈퇴 정상 처리");
+        if (isNotNull(user.getEmail())) {
+            ret.put("code", 100);
+            ret.put("message", "필수 변수값 없음");
+            return ret;
+        }
+        String emailCookie = "";
         //쿠키정보를 체크해서 회원인지를 확인한다. user.email과 쿠키에 있는 이메일이 같은지 확인하다.
+        Cookie[] cookies = request.getCookies();
+        if(cookies != null) {
+            for(Cookie c : cookies){
+                if ("email".equals(c.getName())) {
+                    emailCookie = URLDecoder.decode(c.getValue(), "UTF-8");
+                }
+            }
+        }
         int emailCnt = userService.dupEmail(user);
         if (emailCnt > 0) {
-            //회원탈퇴처리
-            userService.withdraw(user);
-            //회원탈퇴가 제대로 처리 되면 쿠키를 삭제한다.
-        } else {
-            ret.put("code", "101");
-            ret.put("message", "회원정보 없음");
+            if(user.getEmail().equals(emailCookie)) {
+                //회원탈퇴처리
+                userService.withdraw(user);
+                //회원탈퇴가 제대로 처리 되면 쿠키를 삭제한다.
+                if (cookies != null) { // 쿠키가 한개라도 있으면 실행
+                    for (Cookie c : cookies) {
+                        c.setMaxAge(0); // 유효시간을 0으로 설정
+                        c.setPath("/");
+                        response.addCookie(c); // 응답 헤더에 추가
+                    }
+                }
+            }
+            else {
+                ret.put("code", 104);
+                ret.put("message", "접근권한 없음");
+            }
+        }
+        else {
+                ret.put("code", 102);
+                ret.put("message", "아이디 없음");
         }
         return ret;
     }
 
     //회원 정보 수정
     @PostMapping(value = "/edit")
-    public Map<String, String> edit(@RequestBody User user) throws Exception {
-        Map<String, String> ret = new HashMap();
-        ret.put("code", "200");
-        ret.put("message", "조회 정상");
-        int emailCnt = userService.checkEmailPass(user);
+    public Map<String, Object> edit(@RequestBody User user, HttpServletResponse response, HttpServletRequest request) throws Exception {
+        Map<String, Object> ret = new HashMap();
+        ret.put("code", 200);
+        ret.put("message", "회원정보 수정 정상 처리");
+        if(isNotNull(user.getEmail()) ||    // 필수 변수값 없음
+            isNotNull(user.getEmail()) ||
+            isNotNull(user.getEmail()) ||
+            isNotNull(user.getEmail()) ||
+            isNotNull(user.getEmail()) ||
+            isNotNull(user.getEmail())) {
+            ret.put("code", "100");
+            ret.put("message", "필수 변수값 없음");
+            return ret;
+        }
+        String emailCookie = "";
+        //쿠키정보를 체크해서 회원인지를 확인한다. user.email과 쿠키에 있는 이메일이 같은지 확인하다.
+        Cookie[] cookies = request.getCookies();
+        if(cookies != null) {
+            for(Cookie c : cookies){
+                if ("email".equals(c.getName())) {
+                    emailCookie = URLDecoder.decode(c.getValue(), "UTF-8");
+                }
+            }
+        }
+        int emailCnt = userService.checkEmailPass(user); //이메일과 비밀번호가 일치하면 1, 불일치하면 0 반환
         if (emailCnt > 0) {
-            //회원정보 수정처리
-            userService.edit(user);
-            //회원수정이 제대로 처리 되면 쿠키를 삭제한다.
+            if (user.getEmail().equals(emailCookie)) {
+                if (user.getNewPassword() == null || user.getNewPassword().equals("")) {
+                    //회원정보 수정처리
+                    user.setUpdStyle("ONLYINFO");
+                    userService.edit(user);
+                } else { // 비밀번호 변경처리
+                    //userService.editPassword(user);
+                    user.setUpdStyle("WITHPASS");
+                    userService.edit(user);
+                }
+            } else { // 받아온 user의 email과 쿠키에 담겨있는 user의 email정보가 다른 경우
+                ret.put("code", "104");
+                ret.put("message", "접근권한 없음");
+            }
         } else {
-            ret.put("code", "101");
-            ret.put("message", "회원정보 없음");
+            ret.put("code", "103");
+            ret.put("message", "비밀번호 불일치");
         }
         return ret;
     }
 
-    @Value("${key}")
-    private String salt;    // 비밀번호 암호화 키
+    //중복가입 확인
+    @PostMapping(value = "/same")
+    public Map<String, Object> same(@ModelAttribute User user) throws Exception {
+        Map<String, Object> ret = new HashMap();
+        ret.put("code", 200);
+        ret.put("message", "중복가입 확인 정상 처리");
+        if (isNotNull(user.getEmail())) {
+            ret.put("code", 100);
+            ret.put("message", "필수 변수값 없음");
+            return ret;
+        }
+        int deletedUser = userService.deletedUser(user); // 1: 삭제된 유저 0: 삭제되지 않은 유저
+        int emailCnt = userService.dupEmail(user);
+        Map<String, Object> rst = new HashMap();
+        String memberYn = (emailCnt == 1) ? "Y" : "N";
+        rst.put("memberYn", memberYn);
+        if(deletedUser != 0) {
+            ret.put("result",  rst);
+        } else {
+            ret.put("code", 105);
+            ret.put("message", "탈퇴한 회원 아이디");
+        }
+        return ret;
+    }
+
 
     // 로그인
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public Map login(@ModelAttribute User params, HttpServletResponse response) throws Exception {
-        Map<String, Object> rst = new HashMap<String, Object>();
-        User vo = new User();
-        String password = vo.getPassword();
-        password = SHA256Util.getEncrypt(password, salt);
-        vo.setEmail(params.getEmail());
-        vo.setPassword(password);
-        if (vo.getEmail() == null) {
-            rst.put("code", 101);
-            rst.put("messsage", "아이디 없음");
-            return rst;
-        } else if (vo.getPassword() == null) {
-            rst.put("code", 103);
-            rst.put("messsage", "비밀번호 없음");
-            return rst;
+    public Map login(@ModelAttribute User user, HttpServletResponse response) throws Exception {
+        Map<String, Object> ret = new HashMap();
+        if (user.getEmail() == null) {
+            ret.put("code", 101);
+            ret.put("messsage", "아이디 없음");
+            return ret;
+        } else if (user.getPassword() == null) {
+            ret.put("code", 103);
+            ret.put("messsage", "비밀번호 없음");
+            return ret;
         }
+        User vo = new User();
+        String password  = SHA256Util.getEncrypt(user.getPassword(), salt);
+        vo.setEmail(user.getEmail());
+        vo.setPassword(password);
         User login = userService.login(vo);
-        if (login.getPassword() != vo.getPassword()) {
-            rst.put("code", 104);
-            rst.put("messsage", "비밀번호 불일치");
-            return rst;
+        if (!login.getPassword().equals(vo.getPassword())) {
+            ret.put("code", 104);
+            ret.put("messsage", "비밀번호 불일치");
+            return ret;
         }
         if (login == null) {
-            rst.put("code", 200);
-            rst.put("messsage", "로그인 실패");
+            ret.put("code", 200);
+            ret.put("messsage", "로그인 실패");
         } else {
-            Cookie loginCookie = new Cookie("email", login.getEmail());
-            loginCookie.setPath("/");
-            loginCookie.setMaxAge(-1);
-            response.addCookie(loginCookie);
-            rst.put("code", 200);
-            rst.put("messsage", "로그인 성공");
+            Cookie[] loginCookies = new Cookie[4];      // 쿠키 설정
+            loginCookies[0] = new Cookie("email", URLEncoder.encode(login.getEmail(), "UTF-8")); //UTF-8로 인코딩
+            loginCookies[1] = new Cookie("userNo", login.getUserNo() + "");
+            loginCookies[2] = new Cookie("userGbCode", login.getUserGbCode());
+            loginCookies[3] = new Cookie("userName", URLEncoder.encode(login.getUserName(), "UTF-8"));
+            for (Cookie c : loginCookies) {
+                c.setPath("/");
+                c.setMaxAge(-1);
+                response.addCookie(c);
+            }
+            ret.put("code", 200);
+            ret.put("messsage", "로그인 성공");
         }
-        return rst;
+        return ret;
     }
 
     //로그아웃
     @RequestMapping(value = "/logout", method = RequestMethod.GET)
-    public String logout(HttpServletResponse response, HttpServletRequest request) throws Exception {
+    public Map logout(HttpServletResponse response, HttpServletRequest request) throws Exception {
+        Map<String, Object> rst = new HashMap<String, Object>();
         Cookie[] cookies = request.getCookies(); // 모든 쿠키의 정보를 cookies에 저장
         if (cookies != null) { // 쿠키가 한개라도 있으면 실행
             for (int i = 0; i < cookies.length; i++) {
-                cookies[i].setMaxAge(0); // 유효시간을 0으로 설정
+                cookies[i].setMaxAge(0); // 쿠키 유효시간을 0으로 설정
+                cookies[i].setPath("/");
                 response.addCookie(cookies[i]); // 응답 헤더에 추가
+                rst.put("message", "로그아웃 정상 처리");
             }
         }
-        return "redirect:/";
+        rst.put("code", "200");
+        return rst;
     }
 
     @Autowired
@@ -163,19 +304,14 @@ public class UserController {
 
     //  비밀번호 찾기
     @RequestMapping(value = "/findPw", method = RequestMethod.POST)
-    public Map<String, Object> findPw(@ModelAttribute User vo) throws Exception {
-        Map<String, Object> rst = new HashMap<String, Object>();
-        if (vo.getEmail() == null) {
+    public Map<String, Object> findPw(@ModelAttribute User user) throws Exception {
+        Map<String, Object> rst = new HashMap();
+        if (user.getEmail() == null) {
             rst.put("code", 101);
             rst.put("message", "아이디 없음");
             return rst;
         }
-        if (vo.getPassword() == null) {
-            rst.put("code", 102);
-            rst.put("message", "비밀번호 없음");
-            return rst;
-        }
-        int chkNo = userService.findUserNo(vo);
+        int chkNo = userService.findUserNo(user);
         PwSec pwSec = new PwSec();
         pwSec.setUserNo(chkNo);
         pwSec.setSecCode(pwSecService.findCode(pwSec.getUserNo()));
@@ -212,7 +348,7 @@ public class UserController {
             pwSecService.updateCode(pwSec);
         }
 //      메일 발송 부분
-        String to = vo.getEmail(); //받는 사람
+        String to = user.getEmail(); //받는 사람
         String from = ""; //보내는 사람
         String subject = "이 프로젝트의 비밀번호 찾기 메일입니다."; //제목
         String body = "내용@@" + pwSec.getSecCode(); //내용
@@ -232,8 +368,8 @@ public class UserController {
     //비밀번호 재설정
     @RequestMapping(value = "/pwSet", method = RequestMethod.POST)
     public Map<String, Object> setPw(@ModelAttribute User user) throws Exception {
-        Map<String, Object> rst = new HashMap<String, Object>();
-        if (user.getPassword() == null || user.getPassword() == "") {
+        Map<String, Object> rst = new HashMap();
+        if (user.getPassword() == null || user.getPassword().equals("")) {
             rst.put("code", 100);
             rst.put("message", "필수값 없음");
             return rst;

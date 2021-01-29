1ef73f59f95e242c179e8e8dfaa71c26b6f35c96
misonamoo <bananacolor@gmail.com>
2021-01-30 01:26:36 +0900

    war 설정

--- src/main/java/com/misonamoo/niaportal/NiaportalApplication.java
+++ src/main/java/com/misonamoo/niaportal/NiaportalApplication.java
@@ -2,9 +2,16 @@ package com.misonamoo.niaportal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class NiaportalApplication {
+public class NiaportalApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(NiaportalApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(NiaportalApplication.class, args);
